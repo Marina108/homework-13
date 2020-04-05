22934c740b5cdce600456e070c278e3e14227806
Marina108 <60389528+Marina108@users.noreply.github.com>
2020-04-05 17:59:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function getNumber() {
+    let a = prompt('a = ', 1);
+    let b = prompt('b = ', 1);
+    let c = prompt('c = ', 1);
+    let d = b * b - 4 * a *c;
+    alert(d)
+};
+
+getNumber();
